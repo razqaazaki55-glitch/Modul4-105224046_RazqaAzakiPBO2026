a86c97ba14d76fd77f5c61bd9d45896b2a28c084
Razqa <razqaazaki55@gmail.com>
2026-03-31 17:39:28 +0700

    Modul 4

--- PostTest/DuitKuApp.java
+++ PostTest/DuitKuApp.java
@@ -3,11 +3,10 @@ class Pelanggan {
     private String noHP;
     private int saldo;
 
-    // Constructor
     public Pelanggan(String nama, String noHP) {
         this.nama = nama;
         this.noHP = noHP;
-        this.saldo = 0; // saldo awal 0
+        this.saldo = 0;
         System.out.println("Pelanggan berhasil dibuat:");
         System.out.println("Nama: " + nama);
         System.out.println("No HP: " + noHP);
@@ -20,25 +19,24 @@ class Pelanggan {
         System.out.println("Melakukan Top Up: Rp " + jumlah);
 
         if (jumlah < 10000) {
-            System.out.println("❌ Gagal! Minimum top up adalah Rp 10.000");
+            System.out.println("mGagal! Minimum top up adalah Rp 10.000");
         } else {
             saldo += jumlah;
-            System.out.println("✅ Top up berhasil!");
+            System.out.println("Top up berhasil!");
             System.out.println("Saldo sekarang: Rp " + saldo);
         }
         System.out.println();
     }
 
-    // Method Bayar Tagihan
     public void bayarTagihan(int jumlah) {
         System.out.println("Membayar tagihan: Rp " + jumlah);
 
         if (saldo >= jumlah) {
             saldo -= jumlah;
-            System.out.println("✅ Pembayaran berhasil!");
+            System.out.println("Pembayaran berhasil!");
             System.out.println("Sisa saldo: Rp " + saldo);
         } else {
-            System.out.println("❌ Pembayaran gagal! Saldo tidak mencukupi.");
+            System.out.println("Pembayaran gagal! Saldo tidak mencukupi.");
             System.out.println("Saldo saat ini: Rp " + saldo);
         }
         System.out.println();
@@ -49,17 +47,13 @@ class Pelanggan {
     }
 }
 
-// Class utama untuk simulasi
 public class DuitKuApp {
     public static void main(String[] args) {
 
-        // 1. Anton daftar
         Pelanggan anton = new Pelanggan("Anton", "081234567");
 
-        // 2. Top up 50.000
         anton.topUp(50000);
 
-    
         anton.bayarTagihan(60000);
 
         anton.topUp(20000);
